e204173346641517002905d7d12700635adad608
Steffan Harris <contact@steffan.lol>
2022-01-10 23:59:30 +0000

    Added MDX processing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,26 +1,33 @@
 import { graphql } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Blog = ({ data }) => {
   return (
     <Layout pageTitle="Blog">
       Welcome to the blog page
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.id}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
         id
+        body
+        frontmatter {
+          date(formatString: "ddd MMM Do YYYY")
+          title
+        }
       }
     }
   }
