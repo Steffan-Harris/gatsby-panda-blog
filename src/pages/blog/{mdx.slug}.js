17396c90c2a3e336c8abf0f64cef650a74b9a87c
Steffan Harris <steffan.harris@vodafone.com>
2022-01-15 03:23:30 +0000

    Added GatsbyImage to blog post page

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Layout from '../../components/layout'
 import { graphql } from "gatsby";
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const BlogPostPage = ({data}) => {
-  console.log("Data", data)
+  const heroImage = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>Posted on {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={heroImage} alt={data.mdx.frontmatter.hero_image_alt_text}/>
+      <a href={data.mdx.frontmatter.hero_image_url}>
+        <p>Credit: {data.mdx.frontmatter.hero_image_photographer}</p>
+      </a>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -21,6 +26,15 @@ export const query = graphql`
       frontmatter {
         date(formatString: "ddd MMM Do YYYY")
         title
+        hero_image {
+          id
+          childImageSharp {
+            gatsbyImageData(transformOptions: {cropFocus: CENTER})
+          }
+        }
+        hero_image_alt_text
+        hero_image_photographer
+        hero_image_url
       }
       body
     }
